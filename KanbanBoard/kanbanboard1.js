a0d00f149343d27a7020f7acc2d0eb793ac1c08f
surajguduru <guduru.23bcs10012@ms.sst.scaler.com>
2024-01-05 12:04:31 +0530

    On refreshing, the tickets remain there

--- KanbanBoard/kanbanboard1.js
+++ KanbanBoard/kanbanboard1.js
@@ -8,43 +8,39 @@ let lock = document.querySelector('.lock');
 let colorArr = ['red', 'blue', 'green', 'pink'];
 let filterColor = document.querySelectorAll('.color');
 
-for(let i=0;i<filterColor.length;i++){
-    filterColor[i].addEventListener('click',function(){
+for (let i = 0; i < filterColor.length; i++) {
+    filterColor[i].addEventListener('click', function () {
         let color = filterColor[i].classList[1];
         filterTickets(color);
     });
 };
-for(let i=0;i<filterColor.length;i++){
-    filterColor[i].addEventListener('dblclick',function(){
-        filterTickets('black');
-    });
-}
-function filterTickets(color){
-    let tickets = document.querySelectorAll('.ticket-cont');
-    if(color == 'black'){
-        for(let i=0;i<tickets.length;i++){
+for (let i = 0; i < filterColor.length; i++) {
+    filterColor[i].addEventListener('dblclick', function () {
+        let tickets = document.querySelectorAll('.ticket-cont');
+        for (let i = 0; i < tickets.length; i++) {
             tickets[i].style.display = 'block';
         }
-    }
-    else{
-        for(let i=0;i<tickets.length;i++){
-            let ticketColor = tickets[i].querySelector('.ticket-color');
-            if(ticketColor.classList.contains(color)){
-                tickets[i].style.display = 'block';
-            }
-            else{
-                tickets[i].style.display = 'none';
-            }
+    });
+}
+function filterTickets(color) {
+    let tickets = document.querySelectorAll('.ticket-cont');
+    for (let i = 0; i < tickets.length; i++) {
+        let ticketColor = tickets[i].querySelector('.ticket-color');
+        if (ticketColor.classList.contains(color)) {
+            tickets[i].style.display = 'block';
+        }
+        else {
+            tickets[i].style.display = 'none';
         }
     }
 }
 
-addBtn.addEventListener('click', function() {
+addBtn.addEventListener('click', function () {
     console.log("Add-Btn has been clicked")
     if (addModal) {
         modal.style.display = 'flex'; //show modal
-    } 
-    else{
+    }
+    else {
         modal.style.display = 'none'; //hide modal
     }
     addModal = !addModal;
@@ -52,21 +48,21 @@ addBtn.addEventListener('click', function() {
 
 let toolboxColors = document.querySelectorAll('.toolbox-priority-cont .color');
 let modalColors = document.querySelectorAll('.priority-color-cont .priority-color');
-let selectedColor = "red"; 
+let selectedColor = "red";
 
 toolboxColors.forEach(color => {
-    color.addEventListener('click', function() {
-        removeActiveClass(toolboxColors); 
-        selectedColor = color.classList[1]; 
+    color.addEventListener('click', function () {
+        removeActiveClass(toolboxColors);
+        selectedColor = color.classList[1];
         color.classList.add('active');
     });
 });
 
 modalColors.forEach(color => {
-    color.addEventListener('click', function() {
-        removeActiveClass(modalColors); 
-        selectedColor = color.classList[1]; 
-        color.classList.add('active'); 
+    color.addEventListener('click', function () {
+        removeActiveClass(modalColors);
+        selectedColor = color.classList[1];
+        color.classList.add('active');
     })
 });
 
@@ -76,14 +72,14 @@ function removeActiveClass(colors) {
     });
 }
 
-textArea.addEventListener('keydown', function(e) {
+textArea.addEventListener('keydown', function (e) {
     let key = e.key;
     if (key === "Enter") {
         console.log("Enter has been pressed");
-        if(textArea.value == "") {
+        if (textArea.value == "") {
             alert("Please enter some task");
             return;
-        } 
+        }
         generateTicket();
         textArea.value = "";
         modal.style.display = 'none';
@@ -91,45 +87,47 @@ textArea.addEventListener('keydown', function(e) {
     }
 });
 
-function generateTicket() {
-    let ticketCont = document.createElement('div');
-    text = textArea.value;
-    var uid = new ShortUniqueId();
-    ticketCont.classList.add('ticket-cont');
-    ticketCont.innerHTML = `<div class="ticket-color ${selectedColor}"></div>
-    <div class="ticket-id">#${uid.rnd()}</div>
-    <div class="ticket-area">${text}</div>
-    <div class="lock"><i class="fa-solid fa-lock"></i></div>`;
-    mainCont.appendChild(ticketCont);
-    console.log("Ticket has been generated");
+window.addEventListener('DOMContentLoaded', function () {
+    let storedTickets = localStorage.getItem('allTickets');
+    if (storedTickets) {
+        let parsedTickets = JSON.parse(storedTickets);
+        parsedTickets.forEach(ticket => {
+            let ticketCont = document.createElement('div');
+            ticketCont.classList.add('ticket-cont');
+            ticketCont.innerHTML = `<div class="ticket-color ${ticket.ticketColor}"></div>
+            <div class="ticket-id">#${ticket.ticketId}</div>
+            <div class="ticket-area">${ticket.ticketArea}</div>
+            <div class="lock"><i class="fa-solid fa-lock"></i></div>`;
+            mainCont.appendChild(ticketCont);
 
-    //Handle Priority Color
-    let ticketColor = ticketCont.querySelector('.ticket-color');
-    ticketColor.addEventListener('click', function() {
-        let currColor = ticketColor.classList[1];
-        let index = colorArr.indexOf(currColor);
-        let nextIndex = (index + 1) % 4;
-        let nextColor = colorArr[nextIndex];
-        ticketColor.classList.remove(currColor);
-        ticketColor.classList.add(nextColor);
-    });
+            // Handle Priority Color
+            let ticketColor = ticketCont.querySelector('.ticket-color');
+            ticketColor.addEventListener('click', function () {
+                let currColor = ticketColor.classList[1];
+                let index = colorArr.indexOf(currColor);
+                let nextIndex = (index + 1) % 4;
+                let nextColor = colorArr[nextIndex];
+                ticketColor.classList.remove(currColor);
+                ticketColor.classList.add(nextColor);
+            });
 
-    //Handle Lock Button
-    let lock = ticketCont.querySelector('.lock'); 
-    let taskArea = ticketCont.querySelector('.ticket-area');
-    lock.addEventListener('click', function() {
-        let lockIcon = lock.querySelector('i.fa-solid'); 
-        if (lockIcon.classList.contains('fa-lock-open')) {
-            lockIcon.classList.remove('fa-lock-open');
-            lockIcon.classList.add('fa-lock');
-            taskArea.setAttribute('contenteditable', 'false');
-        } 
-        else {
-            lockIcon.classList.remove('fa-lock');
-            lockIcon.classList.add('fa-lock-open');
-            taskArea.setAttribute('contenteditable', 'true');
-        }
-        console.log("Lock-Button has been toggled");
-    });
-}
+            // Handle Lock Button
+            let lock = ticketCont.querySelector('.lock');
+            let taskArea = ticketCont.querySelector('.ticket-area');
+            lock.addEventListener('click', function () {
+                let lockIcon = lock.querySelector('i.fa-solid');
+                if (lockIcon.classList.contains('fa-lock-open')) {
+                    lockIcon.classList.remove('fa-lock-open');
+                    lockIcon.classList.add('fa-lock');
+                    taskArea.setAttribute('contenteditable', 'false');
+                } else {
+                    lockIcon.classList.remove('fa-lock');
+                    lockIcon.classList.add('fa-lock-open');
+                    taskArea.setAttribute('contenteditable', 'true');
+                }
+                console.log("Lock-Button has been toggled");
+            });
+        });
+    }
+});
 
